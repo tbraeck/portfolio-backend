ed944e2b43447eb23f15c3dd3477028174370f5f
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-01-11 19:39:25 -0700

    trying to render still

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
