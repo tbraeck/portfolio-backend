f8d1e1520c92905757b84103d1aeb3b18ada3cca
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-01-14 20:19:41 -0700

    navbar styles

--- client/src/general/NavigationButtons.js
+++ client/src/general/NavigationButtons.js
@@ -13,12 +13,12 @@ const NavigationButtons = () => {
   };
 
   return (
-    <div style={{marginRight: "300px",}}>
-      <button className="button-image" style={{fontSize: "50px", backgroundColor: "#e3e1e1", border: "none", color: "gray",  }} onClick={handleGoBack}>
+    <div style={{marginRight: "200px",}}>
+      <button className="button-image" style={{fontSize: "70px", backgroundColor: "#e3e1e1", border: "none", color: "gray",  }} onClick={handleGoBack}>
       ◀
         {/* <img src='/6.png' style={{width: "60px"}} alt='Go Back' className="button-image" /> */}
       </button>
-      <button className="button-image" style={{fontSize: "50px", backgroundColor: "#e3e1e1", border: "none", color: "gray" }} onClick={handleGoForward}>
+      <button className="button-image" style={{fontSize: "70px", backgroundColor: "#e3e1e1", border: "none", color: "gray" }} onClick={handleGoForward}>
       ▶
         {/* <img src='/5.png'style={{width: "60px", }} alt='Go Forward' className="button-image" /> */}
       </button>
