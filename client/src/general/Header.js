caa59c8145b6bd3d5fb8d5ecf86cfcd63f8c66cc
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-01-21 20:04:47 -0700

    home button fix

--- client/src/general/Header.js
+++ client/src/general/Header.js
@@ -5,7 +5,7 @@ import NavigationButtons from './NavigationButtons'
 const Header = () => {
   return (
   <div className="headerCont">
-    <a href='http://localhost:4000/' className="logo"> <img src="/LOGO.ico" style={{width: "150px", marginLeft: "20px"}}  alt="Portfolio Logo" /></a>  
+    <a href='https://tate-braeckel-web-dev-portfolio.onrender.com/' className="logo"> <img src="/LOGO.ico" style={{width: "150px", marginLeft: "20px"}}  alt="Portfolio Logo" /></a>  
     <div style={{marginLeft: "35px", color: "gray", height: "200px", marginTop: "50px"}}>
       <h1 className='display-5'>TATE BRAECKEL</h1>
       <h1 className='display-7'>FULLSTACK DEVELOPER | DESIGNER</h1>
