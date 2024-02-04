7d80aa73589804133c2fb85974eefdeb058a57db
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-02-03 20:02:06 -0700

    Changing home component, streamline, clean

--- client/src/general/Home.js
+++ client/src/general/Home.js
@@ -1,32 +1,46 @@
 import React from 'react';
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import ApplicationList from '../ApplicationList.js';
 
 const Home = () => {
-  
-  
-
   return (
     <div className="home">
-      <div className="background-image" style={{marginLeft: "50px"}} ></div>
+      {/* <div className="background-image" style={{ marginLeft: "50px" }} ></div> */}
       <div className="content">
         <div className="header-box">
-          <div className="mainPageItemsContainer" key="mainPageItemsContainer" >
-            <div className="card-item" style={{ marginLeft: 'auto', marginRight: '220px', maxWidth: "900px" }}>
+          <div className="mainPageItemsContainer" key="mainPageItemsContainer">
+          {/* <div>
+              <img
+                src='./ME.jpeg'
+                alt='myImage'
+                style={{
+                  width: "250px",
+                  height: "auto",
+                  marginLeft: "200px", // Adjust the marginLeft value
+                  marginTop: "90px",  // Adjust the marginTop value
+                }}
+              />
+            </div> 
+             <div className="card-item" style={{ marginLeft: 'auto', marginRight: '220px', maxWidth: "900px" }}>
               <Link to={`/applications`} className="link header-link">
                 <h1 className='category-column-h1' style={{ textAlign: 'center', marginBottom: "0px" }}>APPLICATIONS</h1>
               </Link>
               <div className="card-container" style={{ display: 'flex', padding: '0px', justifyContent: 'center' }}>
-                <ApplicationList style={{marginRight: "10px"}}/>
-                {/* Add more ApplicationList components as needed */}
+                <ApplicationList style={{ marginRight: "10px" }} />
               </div>
+            </div> */}
+            
+            <div>
+              <h1 className='changa-font'>WEB DEVELOPER</h1>
+            </div>
+            <div>
+              <h1 className='changa-font'>DESIGNER</h1>
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-  
 };
 
 export default Home;
