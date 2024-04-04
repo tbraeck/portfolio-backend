06e1267f1458ba5619fb81a2c933805a5153a03d
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-04-03 19:49:11 -0600

    trying to edit app.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,6 @@ import { Content } from './general/Styles';
 import Home from './general/Home';
 import ApplicationList from './ApplicationList';
 import Blog from './Blog';
-import Developer from './general/Developer';
-import Creator from './general/Creator';
 import About from './About';
 import Footer from './general/Footer';
 import {Routes, Route} from 'react-router-dom';
@@ -17,32 +15,25 @@ const App = () => {
   
   return (
     <div className="App">
-      {/* <NavigationButtons/>
-      <NavBar/> */}
       <div className='mainContainer'>
         <Routes>
           <Route exact path='/' element={<Home/>}/>
           <Route  path='/about' element={<About />}/>
-          <Route path='/developer' element={<Developer />}/>
+          {/* <Route path='/developer' element={<Developer />}/> */}
           {/* <Route path='/designer' element={<Designer />}/> */}
-          <Route path='/creator' element={<Creator />}/>
+          {/* <Route path='/creator' element={<Creator />}/> */}
           <Route  path='/applications' element={<ApplicationList/>}/>
           <Route path="/apps/:id" element={<ApplicationPage  />}/> 
           <Route path="/designs/:id" element={<DesignCard  />}/> 
-
           <Route  path='/blog' element={<Blog />}/>
-
         </Routes>
-             
           <Fragment> 
-            {/* <Heading>GeeksForGeeks</Heading>  */}
             <Content /> 
             <ScrollButton /> 
           </Fragment>
-      </div>
-            
-      <div className='footer--pin'>
-              <Footer/>
+      </div>  
+        <div className='footer--pin'>
+            <Footer/>
         </div>
       </div>
   )
